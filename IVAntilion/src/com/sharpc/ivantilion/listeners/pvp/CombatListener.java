233c23c9658d1345b8d0164afe6dd1a0e3abd871
CodeWalker1337 <jrgolden2011@live.com>
2015-02-22 21:58:10 -0500

    CombatListener if statement

--- IVAntilion/src/com/sharpc/ivantilion/listeners/pvp/CombatListener.java
+++ IVAntilion/src/com/sharpc/ivantilion/listeners/pvp/CombatListener.java
@@ -1,8 +1,25 @@
 package com.sharpc.ivantilion.listeners.pvp;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
 
 public class CombatListener implements Listener
 {
-
+	@EventHandler
+	public void onDamage(EntityDamageByEntityEvent Event)
+	{
+		Entity attacker = Event.getDamager();
+		Entity victim = Event.getEntity();
+		Player defendingPlayer = (Player) victim;
+		Player attackingPlayer = (Player) attacker;
+		if(attacker instanceof Player)
+		{
+			//TODO Set the player to in combat
+		}
+		
+	}
 }
