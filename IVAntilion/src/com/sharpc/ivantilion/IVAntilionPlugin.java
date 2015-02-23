cca9bef800e75e8ffeb6defb70565e73b3af8f3d
mikebjava <Michael@10.0.0.15>
2015-02-23 02:19:25 -0500

    Finalized plugin.

--- IVAntilion/src/com/sharpc/ivantilion/IVAntilionPlugin.java
+++ IVAntilion/src/com/sharpc/ivantilion/IVAntilionPlugin.java
@@ -1,29 +1,45 @@
 package com.sharpc.ivantilion;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.metadata.Metadatable;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.kitteh.vanish.VanishPlugin;
+import org.kitteh.vanish.staticaccess.VanishNoPacket;
 
 import com.massivecraft.factions.Factions;
 import com.sharpc.ivantilion.listeners.pvp.CombatListener;
+import com.sharpc.ivantilion.timer.PvPTimer;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 
 public class IVAntilionPlugin extends JavaPlugin
 {
 
-	public static final String flavoredPrefix = ChatColor.DARK_RED + "[" + ChatColor.DARK_GREEN + "IVAntilion" + ChatColor.DARK_RED + "]" + ChatColor.BLUE;
+	public static final String flavoredPrefix = ChatColor.DARK_RED + "[" + ChatColor.DARK_GREEN + "IVAntilion" + ChatColor.DARK_RED + "] " + ChatColor.BLUE;
 	public static final String unflavoredPrefix = "[IVAntilion] ";
-
+	public static IVAntilionPlugin instance;
 	public static WorldGuardPlugin worldguard;
+	public static VanishPlugin vanish;
 	public static Factions factions;
+	public static PvPTimer pvptimer;
 
 	@Override
 	public void onEnable()
 	{
-
+		instance = this;
+		loadConfig();
 		PluginManager manager = this.getServer().getPluginManager();
 		worldguard = (WorldGuardPlugin) manager.getPlugin("WorldGuard");
 		factions = (Factions) manager.getPlugin("Factions");
+		vanish = (VanishPlugin) manager.getPlugin("VanishNoPacket");
 
 		if (worldguard == null)
 		{
@@ -35,22 +51,96 @@ public class IVAntilionPlugin extends JavaPlugin
 			System.out.println(unflavoredPrefix + "Factions was not found, hooks will not be applied.");
 		}
 
+		if (vanish == null)
+		{
+			System.out.println(unflavoredPrefix + "VanishNoPacket was not found, hooks will not be applied.");
+		}
+
+		pvptimer = new PvPTimer();
+		pvptimer.start();
+
 		manager.registerEvents(new CombatListener(), this);
 	}
 
 	@Override
 	public void onDisable()
 	{
-
+		pvptimer.stop();
 	}
 
 	public void loadConfig()
 	{
 		this.getConfig().addDefault("tagDuration", 5000);
+		this.getConfig().addDefault("tagPVP", true);
+		this.getConfig().addDefault("tagPVE", false);
+		this.getConfig().addDefault("hostileMobsOnly", true);
+		this.getConfig().addDefault("tagGamemode", true);
 		this.getConfig().addDefault("messagePrefix", "&4[&2IVAntilion&4]&9");
 		this.getConfig().addDefault("messagePvPTagged", "&9 You're now in combat. Logging out will result in death.");
 		this.getConfig().addDefault("messagePvPUntagged", "&9 You're no longer in combat.");
+		this.getConfig().addDefault("broadcastPvPLogging", true);
+		this.getConfig().addDefault("broadcast", "&9 %player tried to PvP Logged and was killed!");
 		this.getConfig().options().copyDefaults(true);
 		this.saveConfig();
+	}
+
+	public static Object getMetadata(Metadatable object, String key)
+	{
+		List<MetadataValue> values = object.getMetadata(key);
+		for (MetadataValue value : values)
+		{
+			if (instance != null)
+			{
+				if (value.getOwningPlugin() == instance)
+				{
+					return value.value();
+				}
+			}
+		}
+		return null;
+	}
+
+	public static String replaceColors(String message)
+	{
+		return message.replaceAll("(?i)&([a-f0-9])", "\u00A7$1");
+	}
+
+	public static void broadcastPvPLog(Player playerThatLeft)
+	{
+		instance.getServer().broadcastMessage(flavoredPrefix + replaceColors(instance.getConfig().getString("broadcast").replaceAll("%player", playerThatLeft.getName())));
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		Player player = null;
+		if (sender instanceof Player)
+		{
+			player = (Player) sender;
+
+			if (label.equalsIgnoreCase("ivantilion"))
+			{
+				if (player.isOp())
+				{
+					if (args.length == 0)
+					{
+						player.sendMessage(flavoredPrefix + " v" + this.getDescription().getVersion());
+						player.sendMessage(ChatColor.BLUE + "/ivantilion reload");
+					} else
+					{
+						if (args.length == 1)
+						{
+							if (args[0].equalsIgnoreCase("reload"))
+							{
+								player.sendMessage(flavoredPrefix + "Plugin reloaded.");
+								reloadConfig();
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return false;
 	}
 }
--- IVAntilion/src/com/sharpc/ivantilion/listeners/pvp/CombatListener.java
+++ IVAntilion/src/com/sharpc/ivantilion/listeners/pvp/CombatListener.java
@@ -1,24 +1,289 @@
 package com.sharpc.ivantilion.listeners.pvp;
 
+import java.util.Map;
+
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+
+import com.massivecraft.factions.Rel;
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.MFlag;
+import com.massivecraft.factions.entity.MPlayer;
+import com.sharpc.ivantilion.IVAntilionPlugin;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class CombatListener implements Listener
 {
 	@EventHandler
-	public void onDamage(EntityDamageByEntityEvent Event)
+	public void onDamage(EntityDamageByEntityEvent event)
 	{
-		Entity attacker = Event.getDamager();
-		Entity victim = Event.getEntity();
-		Player defendingPlayer = (Player) victim;
-		Player attackingPlayer = (Player) attacker;
+		Entity attacker = event.getDamager();
+		Entity victim = event.getEntity();
+		Player defendingPlayer = null;
+		Player attackingPlayer = null;
+
 		if (attacker instanceof Player)
 		{
-			//TODO Set the player to in combat 
+			attackingPlayer = (Player) attacker;
 		}
 
+		if (victim instanceof Player)
+		{
+			defendingPlayer = (Player) victim;
+		}
+
+		if (IVAntilionPlugin.vanish != null)
+		{
+			if (attackingPlayer != null && IVAntilionPlugin.vanish.getManager().isVanished(attackingPlayer))
+			{
+				return;
+			}
+
+			if (defendingPlayer != null && IVAntilionPlugin.vanish.getManager().isVanished(defendingPlayer))
+			{
+				return;
+			}
+		}
+
+		// Only check this block if PVE is enabled, as PVE is the only focus of this block.
+		if (IVAntilionPlugin.instance.getConfig().getBoolean("tagPVE"))
+		{
+			if (!this.isPlayerInInvincibleRegion(attackingPlayer))
+			{
+				// If the attacker is a player and the defender is a mob.
+				if (attackingPlayer != null && defendingPlayer == null)
+				{
+					if (victim instanceof Monster || victim instanceof Wolf)
+					{
+						putPlayerInCombat(attackingPlayer);
+					} else if (victim instanceof Animals)
+					{
+						if (!IVAntilionPlugin.instance.getConfig().getBoolean("hostileMobsOnly"))
+						{
+							putPlayerInCombat(attackingPlayer);
+						}
+					}
+				}
+
+				// If the attacker is a mob and the defender is a player.
+				if (attackingPlayer == null && defendingPlayer != null)
+				{
+					if (attacker instanceof Monster)
+					{
+						putPlayerInCombat(attackingPlayer);
+					} else if (attacker instanceof Animals)
+					{
+						if (attacker instanceof Wolf)
+						{
+							Wolf wolf = (Wolf) attacker;
+							if (!IVAntilionPlugin.instance.getConfig().getBoolean("hostileMobsOnly") && wolf.isAngry())
+							{
+								putPlayerInCombat(defendingPlayer);
+							}
+						}
+					}
+				}
+			}
+
+		}
+
+		// Only check this block if PVP is enabled, as PVP is the only focus of this block.
+		if (IVAntilionPlugin.instance.getConfig().getBoolean("tagPVP"))
+		{
+			if (attackingPlayer != null && defendingPlayer != null)
+			{
+				if (this.canFactionAttack(attackingPlayer, defendingPlayer))
+				{
+					if (!this.isPlayerInAntiPVPRegion(attackingPlayer) && !this.isPlayerInAntiPVPRegion(defendingPlayer))
+					{
+						if (attackingPlayer != null && defendingPlayer != null)
+						{
+							putPlayerInCombat(attackingPlayer);
+							putPlayerInCombat(defendingPlayer);
+						}
+					}
+				}
+			}
+		}
 	}
+
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event)
+	{
+		Player player = event.getPlayer();
+
+		if (!player.isDead())
+		{
+			Object value = IVAntilionPlugin.getMetadata(player, "combattag");
+
+			if (value instanceof Boolean)
+			{
+				boolean combat = (boolean) value;
+
+				if (combat == true)
+				{
+					if (IVAntilionPlugin.instance != null && IVAntilionPlugin.instance.getConfig().getBoolean("broadcastPvPLogging"))
+					{
+						IVAntilionPlugin.broadcastPvPLog(player);
+					}
+					player.setHealth(0.0);
+					takePlayerOutOfCombat(player);
+				}
+			}
+
+		}
+	}
+
+	public static void putPlayerInCombat(Player player)
+	{
+		if (IVAntilionPlugin.getMetadata(player, "combattag") != null)
+		{
+			if ((boolean) IVAntilionPlugin.getMetadata(player, "combattag") == false)
+			{
+				player.sendMessage(IVAntilionPlugin.replaceColors(IVAntilionPlugin.instance.getConfig().getString("messagePrefix") + IVAntilionPlugin.instance.getConfig().getString("messagePvPTagged")));
+			}
+		} else
+		{
+			player.sendMessage(IVAntilionPlugin.replaceColors(IVAntilionPlugin.instance.getConfig().getString("messagePrefix") + IVAntilionPlugin.instance.getConfig().getString("messagePvPTagged")));
+		}
+
+		player.setMetadata("combattag", new FixedMetadataValue(IVAntilionPlugin.instance, true));
+		player.setMetadata("combattimeleft", new FixedMetadataValue(IVAntilionPlugin.instance, IVAntilionPlugin.instance.getConfig().getInt("tagDuration")));
+	}
+
+	public static void takePlayerOutOfCombat(Player player)
+	{
+		if (IVAntilionPlugin.getMetadata(player, "combattag") != null)
+		{
+			if ((boolean) IVAntilionPlugin.getMetadata(player, "combattag") == true)
+			{
+				player.sendMessage(IVAntilionPlugin.replaceColors(IVAntilionPlugin.instance.getConfig().getString("messagePrefix") + IVAntilionPlugin.instance.getConfig().getString("messagePvPUntagged")));
+			}
+		} else
+		{
+			player.sendMessage(IVAntilionPlugin.replaceColors(IVAntilionPlugin.instance.getConfig().getString("messagePrefix") + IVAntilionPlugin.instance.getConfig().getString("messagePvPUntagged")));
+		}
+
+		player.setMetadata("combattag", new FixedMetadataValue(IVAntilionPlugin.instance, false));
+		player.setMetadata("combattimeleft", new FixedMetadataValue(IVAntilionPlugin.instance, 0));
+	}
+
+	private boolean isPlayerInAntiPVPRegion(Player player)
+	{
+		if (IVAntilionPlugin.worldguard != null)
+		{
+			for (Map.Entry<String, ProtectedRegion> regionEntry : IVAntilionPlugin.worldguard.getRegionManager(player.getWorld()).getRegions().entrySet())
+			{
+				if (regionEntry.getValue().contains(player.getLocation().getBlockX(), player.getLocation().getBlockY(), player.getLocation().getBlockZ()))
+				{
+					for (Map.Entry<Flag<?>, Object> flagEntry : regionEntry.getValue().getFlags().entrySet())
+					{
+						if (flagEntry.getKey().getName().equalsIgnoreCase("pvp"))
+						{
+							if (flagEntry.getValue().toString().equalsIgnoreCase("deny"))
+							{
+								return true;
+							}
+						} else if (flagEntry.getKey().getName().equalsIgnoreCase("invincible"))
+						{
+							if (flagEntry.getValue().toString().equalsIgnoreCase("allow"))
+							{
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private boolean isPlayerInInvincibleRegion(Player player)
+	{
+		if (IVAntilionPlugin.worldguard != null)
+		{
+			IVAntilionPlugin.worldguard.getRegionManager(player.getWorld()).getRegions();
+			for (Map.Entry<String, ProtectedRegion> regionEntry : IVAntilionPlugin.worldguard.getRegionManager(player.getWorld()).getRegions().entrySet())
+			{
+				if (regionEntry.getValue().contains(player.getLocation().getBlockX(), player.getLocation().getBlockY(), player.getLocation().getBlockZ()))
+				{
+					for (Map.Entry<Flag<?>, Object> flagEntry : regionEntry.getValue().getFlags().entrySet())
+					{
+						if (flagEntry.getKey().getName().equalsIgnoreCase("invincible"))
+						{
+							if (flagEntry.getValue().toString().equalsIgnoreCase("allow"))
+							{
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private boolean canFactionAttack(Player attackingPlayer, Player defendingPlayer)
+	{
+		if (IVAntilionPlugin.factions != null)
+		{
+			MPlayer attackerM = MPlayer.get(attackingPlayer);
+			MPlayer defendingM = MPlayer.get(defendingPlayer);
+			Faction attackerFaction = attackerM.getFaction();
+			Faction defendingFaction = defendingM.getFaction();
+
+			if (attackerFaction != null && defendingFaction != null)
+			{
+				if (attackerFaction.getRelationTo(defendingFaction).isAtLeast(Rel.ALLY))
+				{
+					return false;
+				}
+
+				System.out.println(attackerFaction.getName());
+				System.out.println(defendingFaction.getName());
+
+				if (attackerFaction.getName().equalsIgnoreCase(defendingFaction.getName()))
+				{
+					return false;
+				}
+
+				for (Map.Entry<MFlag, Boolean> entry : attackerFaction.getFlags().entrySet())
+				{
+					if (entry.getKey().equals(MFlag.ID_PEACEFUL))
+					{
+						if (entry.getValue() == true)
+						{
+							return false;
+						}
+					}
+				}
+
+				for (Map.Entry<MFlag, Boolean> entry : defendingFaction.getFlags().entrySet())
+				{
+					if (entry.getKey().equals(MFlag.ID_PEACEFUL))
+					{
+						if (entry.getValue() == true)
+						{
+							return false;
+						}
+					}
+				}
+			}
+
+		}
+
+		return true;
+	}
+
 }
